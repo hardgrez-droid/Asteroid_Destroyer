762c94313c7028f008258c56cbb349bfbfaca8bb
HardGrez <Hardgrez@gmail.com>
2026-03-24 21:15:40 +0100

    Add full project files

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
-from logger import log_state
-
+from logger import log_state, log_event
+from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+from shot import Shot
+import sys
 
 def main():
     print("Starting Asteroids with pygame version: " + pygame.version.ver)
@@ -11,6 +15,24 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers =(updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, drawable, updatable)
+
     while True:
         log_state()
 
@@ -18,8 +40,22 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000  # Limit to 60 FPS and get delta time in seconds
+        
 
 
 if __name__ == "__main__":
